139ba88f5138e6bfd4152508e8057f7fa4e7bfa2
Miggy Go <miguelivann1129@gmail.com>
2026-03-10 09:52:46 +0800

    Added baseline for tests to ride-posting branch

--- bluepool/rideposting/tests.py
+++ bluepool/rideposting/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Ride
 
 # Create your tests here.
+class RideTests(TestCase):
+    def testRide(self):
+        sampleRide = Ride.objects.create(time = timezone.now(), pick_up_location = "here", drop_off_location = "there")
+        self.assertTrue(isinstance(sampleRide,Ride))
+        # self.assertEqual(sampleRide.time, timezone.now())
+        self.assertEqual(sampleRide.pick_up_location, "here")
+        self.assertEqual(sampleRide.drop_off_location, "there")
