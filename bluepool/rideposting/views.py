d0c2f4a9cf543d19bd6f8e7902e67918874fce88
01111000x <jancel.ensina@gmail.com>
2026-03-13 01:33:24 +0800

    fixed import messages

--- bluepool/rideposting/views.py
+++ bluepool/rideposting/views.py
@@ -1,4 +1,4 @@
-from pyexpat.errors import messages
+from django.contrib import messages 
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Ride, RideRequest
