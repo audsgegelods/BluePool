60e4da0b6dacbf3e39e085db7dcf6d54c90c5245
auby <joaquin.tirona@student.ateneo.edu>
2026-04-15 10:28:43 +0800

    modified Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import AppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+import Container from "@mui/material/Container"
+
 function Home() {
-    return <div>Home</div>
+    return ( 
+    <AppBar position='static' color="rgba(161, 168, 168, 0.96)">
+        <Container maxWidth="x1">
+             <Toolbar disableGutters>
+            <h1>yo</h1>
+            </Toolbar>
+        </Container>
+    </AppBar>
+    )
 }
 
 export default Home
