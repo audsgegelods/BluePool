bf63ade101bbdd512e3fa772989f424be204be73
Miggy Go <miguelivann1129@gmail.com>
2026-04-20 23:50:49 +0800

    Updated requirements.txt and settings.py for additional installations

--- bluepool/bluepool/settings.py
+++ bluepool/bluepool/settings.py
@@ -69,7 +69,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.common.CommonMiddleware'
+    'django.middleware.common.CommonMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 
