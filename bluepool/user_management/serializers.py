04078b4f459f6ad475130a53a32d4d489ee861e9
furudooru <160915885+furudooru@users.noreply.github.com>
2026-04-20 20:18:16 +0800

    fix user registration password bug

--- bluepool/user_management/serializers.py
+++ bluepool/user_management/serializers.py
@@ -15,23 +15,14 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "first_name", "last_name", "email", "password"]
         extra_kwargs = {"password": {"write_only": True}}   # do not return password when requesting user data
 
-        def create(self, validated_data):
+    def create(self, validated_data):
             email_address = validated_data["email"]
             new_user = User.objects.create_user(
                 username=validated_data.get("username", ""),
                 email=email_address,
                 first_name=validated_data.get("first_name", ""),
                 last_name=validated_data.get("last_name", ""),
-                password=validated_data["password"]
             )
-            # new_profile = Profile.objects.create(
-            #     user=new_user,
-            #     name=validated_data.get("username", None),
-            #     email_address=validated_data.get("email", None))
-            # new_profile.save()
+            new_user.set_password(validated_data["password"])
+            new_user.save()
             return new_user
-
-# class ProfileSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Profile
-#         fields
